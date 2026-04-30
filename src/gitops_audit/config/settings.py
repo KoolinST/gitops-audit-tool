1c359254bb4811ab7dd259fa321bd64409ff0c7b
Pavlo <largeeps@gmail.com>
2026-04-30 14:14:43 +0200

    drop scaffolding comments

--- src/gitops_audit/config/settings.py
+++ src/gitops_audit/config/settings.py
@@ -8,22 +8,11 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    # Database
     database_url: str = "postgresql+asyncpg://postgres:password@localhost:5432/gitops_audit"
-
-    # GitHub
     github_token: str = ""
-
-    # Prometheus
     prometheus_url: str = "http://localhost:9090"
-
-    # ArgoCD
     argocd_url: str = "http://localhost:8080"
-
-    # Logging
     log_level: str = "INFO"
-
-    # Slack
     slack_webhook_url: str = ""
 
 
